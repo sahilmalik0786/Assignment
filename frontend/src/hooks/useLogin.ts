249dcfa3dae7b7c210b603370639d249e9ff35e8
sahilmalik0786 <rahisu78@gmail.com>
2025-09-01 14:44:11 +0530

    refactor: Comment out unused queryClient import and related code in useLogin hook

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query"
 import {  loginWithOtp } from "../features/queries"
 
 import type { FormDataLogin } from "../components/login-form"
-import { queryClient } from "../queryClient"
+// import { queryClient } from "../queryClient"
 import { useNavigate } from "@tanstack/react-router"
 
 export const useLogin = ()=>{
@@ -10,7 +10,7 @@ export const useLogin = ()=>{
     return useMutation({
         mutationFn:(credential:FormDataLogin)=>loginWithOtp(credential),
         onSuccess: async()=>{
-            await queryClient.invalidateQueries({queryKey:['me']})
+            // await queryClient.invalidateQueries({queryKey:['me']})
             navigate({to:'/dashboard'})
         }
     })
