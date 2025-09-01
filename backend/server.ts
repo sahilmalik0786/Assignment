8c7b1cab5b424337ab7bf900417c8bfe45673195
sahilmalik0786 <rahisu78@gmail.com>
2025-09-01 10:13:20 +0530

    fix: Update server and app scripts for consistency and add missing environment import

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,6 @@
-import './src/env.js'
 import app from './src/app.js'
 
 
 
 
 const PORT:Number = 3000
-app.listen(PORT , ()=>{
-    console.log('hello server is runnig on port ')
-})
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import './env.js'
 import  express  from "express";
 import type { Application } from "express";
 import connectDB from "./config/db.js";
@@ -8,7 +9,10 @@ import authRoutes from './routes/user.routes.js'
 import notesRoutes from './routes/notes.routes.js'
 
 const app:Application = express()
+const PORT:number = 3000
 connectDB()
+
+
 
 app.use(express.json())
 app.use(cookieparser())
@@ -23,4 +27,6 @@ app.use('/api/auth', authRoutes)
 //notes routes will handle with this
 app.use('/api/notes' , notesRoutes)
 
-export default app 
+app.listen(PORT , ()=>{
+    console.log('hello server is runnig on port ')
+})
